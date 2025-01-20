997643974422fa3a2f6f9b3e2508083aa8874efa
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-20 19:10:34 +0300

    add annotation EnableFeignClients

--- src/main/java/org/project/booktrackerservice/BookTrackerServiceApplication.java
+++ src/main/java/org/project/booktrackerservice/BookTrackerServiceApplication.java
@@ -2,8 +2,10 @@ package org.project.booktrackerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BookTrackerServiceApplication {
 
     public static void main(String[] args) {
