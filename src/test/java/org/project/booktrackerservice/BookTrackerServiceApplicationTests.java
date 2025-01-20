e1f6c7a5f978558ae1e26494225139f2c387f38b
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-20 16:57:08 +0300

    start project

---
+++ src/test/java/org/project/booktrackerservice/BookTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.booktrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
