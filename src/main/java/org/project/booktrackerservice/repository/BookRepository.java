f256bc4fc7b2c454c7bb6d4a1904206e334cfb4d
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-20 23:27:05 +0300

    find book bt book_id

--- src/main/java/org/project/booktrackerservice/repository/BookRepository.java
+++ src/main/java/org/project/booktrackerservice/repository/BookRepository.java
@@ -3,5 +3,8 @@ package org.project.booktrackerservice.repository;
 import org.project.booktrackerservice.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+   Optional<BookEntity> findByBookId(Long id);
 }
