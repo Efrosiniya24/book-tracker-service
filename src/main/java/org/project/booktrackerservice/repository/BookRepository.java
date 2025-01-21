299488e9cfeded77d837c125c1373d8904d09e9b
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-21 11:18:18 +0300

    find deleted books

--- src/main/java/org/project/booktrackerservice/repository/BookRepository.java
+++ src/main/java/org/project/booktrackerservice/repository/BookRepository.java
@@ -3,8 +3,9 @@ package org.project.booktrackerservice.repository;
 import org.project.booktrackerservice.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
    Optional<BookEntity> findByBookId(Long id);
-}
+   List<BookEntity> findAllByIsDeletedFalse();}
