9868a43dae6dd69efb4ec2d55414b8ea06878a00
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-21 22:17:25 +0300

    get free books test

--- src/test/java/org/project/booktrackerservice/controllers/BookTrackerControllerTest.java
+++ src/test/java/org/project/booktrackerservice/controllers/BookTrackerControllerTest.java
@@ -2,15 +2,21 @@ package org.project.booktrackerservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.project.booktrackerservice.Enum.StatusEnum;
+import org.project.booktrackerservice.dto.BookDTO;
 import org.project.booktrackerservice.service.BookTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookTrackerController.class)
@@ -45,5 +51,25 @@ class BookTrackerControllerTest {
                 .andExpect(status().isOk());
 
         verify(bookTrackerService, times(1)).softDeleteBook(1L);
+    }
+
+    @Test
+    void getFreeBooks() throws Exception {
+        //given
+        List<BookDTO> books = Arrays.asList(
+                new BookDTO(1L, 1L, StatusEnum.FREE, LocalDateTime.now(), LocalDateTime.now(), false),
+                new BookDTO(2L, 2L, StatusEnum.FREE, LocalDateTime.now(), LocalDateTime.now(), false)
+        );
+
+        //when
+        when(bookTrackerService.getFreeBooks()).thenReturn(books);
+
+        //then
+        mockMvc.perform(get("/books/book-tracker/free-books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(books.toString()))
+                .andExpect(status().isOk());
+
+        verify(bookTrackerService, times(1)).getFreeBooks();
     }
 }
