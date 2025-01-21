eefe32aa73d87465f52dd1465c5dbdf38f9c9cf1
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-21 11:18:34 +0300

    find deleted books

--- src/main/java/org/project/booktrackerservice/service/BookTrackerService.java
+++ src/main/java/org/project/booktrackerservice/service/BookTrackerService.java
@@ -3,15 +3,20 @@ package org.project.booktrackerservice.service;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.project.booktrackerservice.Enum.StatusEnum;
+import org.project.booktrackerservice.dto.BookDTO;
 import org.project.booktrackerservice.entity.BookEntity;
+import org.project.booktrackerservice.mapper.BookMapper;
 import org.project.booktrackerservice.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BookTrackerService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     public void createBook(Long id) {
         BookEntity bookEntity = new BookEntity();
@@ -26,5 +31,10 @@ public class BookTrackerService {
                 .orElseThrow(() -> new EntityNotFoundException("Book with id = " + id + " not found"));
         bookEntity.setDeleted(true);
         bookRepository.save(bookEntity);
+    }
+
+    public List<BookDTO> getFreeBooks() {
+        List<BookEntity> freeBooks = bookRepository.findAllByIsDeletedFalse();
+        return bookMapper.toBookDTOList(freeBooks);
     }
 }
