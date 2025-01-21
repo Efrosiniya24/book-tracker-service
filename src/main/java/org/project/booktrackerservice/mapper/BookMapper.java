95876f96c2bdbfadbb8aaff01dcb85c93636fcdc
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-21 11:17:45 +0300

    list mapper

--- src/main/java/org/project/booktrackerservice/mapper/BookMapper.java
+++ src/main/java/org/project/booktrackerservice/mapper/BookMapper.java
@@ -1,12 +1,13 @@
 package org.project.booktrackerservice.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 import org.project.booktrackerservice.dto.BookDTO;
 import org.project.booktrackerservice.entity.BookEntity;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface BookMapper {
-    BookEntity toBookEntity(BookDTO bookDTO);
-    BookDTO toBookDTO(BookEntity bookEntity);
+    List<BookDTO> toBookDTOList(List<BookEntity> bookEntities);
+
 }
