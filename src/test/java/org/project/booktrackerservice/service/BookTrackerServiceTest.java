d0d9a4513a124fac62f5e6f6b72e1f7170251634
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-22 11:15:31 +0300

    create book test

---
+++ src/test/java/org/project/booktrackerservice/service/BookTrackerServiceTest.java
@@ -0,0 +1,50 @@
+package org.project.booktrackerservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.project.booktrackerservice.Enum.StatusEnum;
+import org.project.booktrackerservice.dto.BookDTO;
+import org.project.booktrackerservice.entity.BookEntity;
+import org.project.booktrackerservice.mapper.BookMapper;
+import org.project.booktrackerservice.repository.BookRepository;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookTrackerServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private BookMapper bookMapper;
+
+    @InjectMocks
+    private BookTrackerService bookTrackerService;
+
+    @Test
+    void createBookTest() {
+        // given
+        Long bookId = 1L;
+        BookEntity bookEntity = new BookEntity(bookId, bookId, StatusEnum.FREE, LocalDateTime.now(), LocalDateTime.now(), false);
+
+        BookDTO bookDTO = new BookDTO(bookId, bookId, StatusEnum.FREE, LocalDateTime.now(), LocalDateTime.now(), false);
+
+        // when
+        when(bookRepository.save(any(BookEntity.class))).thenReturn(bookEntity);
+        when(bookMapper.toBookDTO(bookEntity)).thenReturn(bookDTO);
+
+        // then
+        BookDTO actualBookDTO = bookTrackerService.createBook(bookId);
+        assertEquals(bookDTO, actualBookDTO);
+
+        verify(bookMapper, times(1)).toBookDTO(bookEntity);
+    }
+
+}
