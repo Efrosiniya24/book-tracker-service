a53ff9d8af6f53bc07bacab65ddcb95094939f94
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-23 21:11:58 +0300

    add swagger config

---
+++ src/main/java/org/project/booktrackerservice/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.project.booktrackerservice.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openapi() {
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8081")
+                        )
+                )
+                .info(
+                        new Info().title("Book Documentation").version("1.0")
+                );
+    }
+}
